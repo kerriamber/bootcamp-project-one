c3f60769e6e5f8880e2e8166250b6574b1ab5126
Kerri Salciccioli <kerri@salciccio.li>
2024-11-22 15:07:55 -0500

    in progress commit

---
+++ assets/js/calendar.js
@@ -0,0 +1,24 @@
+const MONTHS = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+const DAYS = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
